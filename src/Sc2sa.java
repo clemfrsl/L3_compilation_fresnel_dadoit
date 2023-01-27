03c67e3ba5d914472c034ed1381474369be2f146
f20008012 <clem.fresnel@gmail.com>
2023-01-27 17:32:06 +0100

    debut machin

--- src/Sc2sa.java
+++ src/Sc2sa.java
@@ -23,20 +23,160 @@ public class Sc2sa extends DepthFirstAdapter
 	return this.returnValue;
     }
 
-
-    // un exemple de méthode associé à la règle 
-    // exp3 = {plus} exp3 plus exp4 
+    //ldv = {listedeclarationvariable} dv ldvbis
     @Override
-    public void caseAPlusExp3(APlusExp3 node)
+    public void caseAListedeclarationvariableLdv(AListedeclarationvariableLdv node)
     {
-	SaExp op1 = null;
-	SaExp op2 = null;
-        inAPlusExp3(node);
-	node.getExp3().apply(this);
-	op1 = (SaExp) this.returnValue;
-	node.getExp4().apply(this);
-	op2 = (SaExp) this.returnValue;
-	this.returnValue = new SaExpAdd(op1, op2);
-        outAPlusExp3(node);
+        SaExp op1 = null;
+        SaExp op2 = null;
+        inAListedeclarationvariableLdv(node);
+        if(node.getDv() != null)
+        {
+            node.getDv().apply(this);
+            op1 = (SaExp) this.returnValue;
+        }
+        if(node.getLdvbis() != null)
+        {
+            node.getLdvbis().apply(this);
+            op2 = (SaExp) this.returnValue;
+        }
+        this.returnValue = new SaExpAdd(op1, op2);
+        outAListedeclarationvariableLdv(node);
     }
+
+    //ldvbis = {listedeclarationvariablebis} virgule dv ldvbis
+    @Override
+    public void caseAListedeclarationvariablebisLdvbis(AListedeclarationvariablebisLdvbis node)
+    {
+        SaExp op1 = null;
+        SaExp op2 = null;
+        inAListedeclarationvariablebisLdvbis(node);
+        if(node.getDv() != null)
+        {
+            node.getDv().apply(this);
+            op1 = (SaExp) this.returnValue;
+        }
+        if(node.getLdvbis() != null)
+        {
+            node.getLdvbis().apply(this);
+            op2 = (SaExp) this.returnValue;
+        }
+        this.returnValue = new SaExpAdd(op1, op2);
+        outAListedeclarationvariablebisLdvbis(node);
+    }
+
+    //dv = {declarationvariableid} tv identif
+    @Override
+    public void caseADeclarationvariableidDv(ADeclarationvariableidDv node)
+    {
+        SaExp op1 = null;
+        SaExp op2 = null;
+        inADeclarationvariableidDv(node);
+        if(node.getTv() != null)
+        {
+            node.getTv().apply(this);
+            op1 = (SaExp) this.returnValue;
+        }
+        this.returnValue = new SaExpAdd(op1, op2);
+        outADeclarationvariableidDv(node);
+    }
+
+    //{declarationvariableidtableau} tv identif crochetgauche nombre crochetdroit;
+    @Override
+    public void caseADeclarationvariableidtableauDv(ADeclarationvariableidtableauDv node)
+    {
+        SaExp op1 = null;
+        SaExp op2 = null;
+        inADeclarationvariableidtableauDv(node);
+        if(node.getTv() != null)
+        {
+            node.getTv().apply(this);
+            op1 = (SaExp) this.returnValue;
+        }
+        this.returnValue = new SaExpAdd(op1, op2);
+        outADeclarationvariableidtableauDv(node);
+    }
+
+    //ldf = {listedeclarationfonction} df ldf
+    @Override
+    public void caseAListedeclarationfonctionLdf(AListedeclarationfonctionLdf node)
+    {
+        SaExp op1 = null;
+        SaExp op2 = null;
+        inAListedeclarationfonctionLdf(node);
+        if(node.getDf() != null)
+        {
+            node.getDf().apply(this);
+            op1 = (SaExp) this.returnValue;
+        }
+        if(node.getLdf() != null)
+        {
+            node.getLdf().apply(this);
+            op2 = (SaExp) this.returnValue;
+        }
+        this.returnValue = new SaExpAdd(op1, op2);
+        outAListedeclarationfonctionLdf(node);
+    }
+
+
+
+    //exp = {ou} exp ou e1
+    @Override
+    public void caseAOuExp(AOuExp node)
+    {
+        SaExp op1 = null;
+        SaExp op2 = null;
+        inAOuExp(node);
+        if(node.getExp() != null)
+        {
+            node.getExp().apply(this);
+            op1 = (SaExp) this.returnValue;
+        }
+        if(node.getE1() != null)
+        {
+            node.getE1().apply(this);
+            op2 = (SaExp) this.returnValue;
+        }
+        this.returnValue = new SaExpAdd(op1, op2);
+        outAOuExp(node);
+    }
+
+    //exp = {pasou} e1
+    @Override
+    public void caseAPasouExp(APasouExp node)
+    {
+        SaExp op1 = null;
+        inAPasouExp(node);
+        if(node.getE1() != null)
+        {
+            node.getE1().apply(this);
+            op1 = (SaExp) this.returnValue;
+        }
+        this.returnValue = op1;
+        outAPasouExp(node);
+    }
+
+    //e3 = {plus} e3 plus e4
+    @Override
+    public void caseAPlusE3(APlusE3 node)
+    {
+        SaExp op1 = null;
+        SaExp op2 = null;
+        inAPlusE3(node);
+        if(node.getE3() != null)
+        {
+            node.getE3().apply(this);
+            op1 = (SaExp) this.returnValue;
+        }
+
+        if(node.getE4() != null)
+        {
+            node.getE4().apply(this);
+            op2 = (SaExp) this.returnValue;
+        }
+        this.returnValue = new SaExpAdd(op1, op2);
+        outAPlusE3(node);
+    }
+
+
 }
