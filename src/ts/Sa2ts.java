0e192798026b825188745cb62973c90f526ab65d
d21218463 <juliette.dadoit@etu.univ-amu.fr>
2023-03-24 16:31:24 +0100

    TP3 reussi

--- src/ts/Sa2ts.java
+++ src/ts/Sa2ts.java
@@ -1,85 +1,83 @@
 package ts;
 import sa.*;
 import util.Error;
+import util.Type;
 
 public class Sa2ts extends SaDepthFirstVisitor <Void> {
-    enum Context {
-	LOCAL,
-	GLOBAL,
-	PARAM
-    }
-    
-    private Ts tableGlobale;
-    private Ts tableLocaleCourante;
-    private Context context;
-    
-    public Ts getTableGlobale(){return this.tableGlobale;}
+	enum Context {
+		LOCAL,
+		GLOBAL,
+		PARAM
+	}
 
-    public Sa2ts(SaNode root)
-    {
-	tableGlobale = new Ts();
-	tableLocaleCourante = null;
-	context = Context.GLOBAL;
-	try{
-	    root.accept(this);
-	    if(tableGlobale.getFct("main") == null)
-		throw new ErrorException(Error.TS, "la fonction main n'est pas définie");
+	private Ts tableGlobale;
+	private Ts tableLocaleCourante;
+	private Context context;
+
+	public Ts getTableGlobale(){return this.tableGlobale;}
+
+	public Sa2ts(SaNode root)
+	{
+		tableGlobale = new Ts();
+		tableLocaleCourante = null;
+		context = Context.GLOBAL;
+		try{
+			root.accept(this);
+			if(tableGlobale.getFct("main") == null)
+				throw new ErrorException(Error.TS, "la fonction main n'est pas définie");
+		}
+		catch(ErrorException e){
+			System.err.print("ERREUR TABLE DES SYMBOLES : ");
+			System.err.println(e.getMessage());
+			System.exit(e.getCode());
+		}
+		catch(Exception e){}
 	}
-	catch(ErrorException e){
-	    System.err.print("ERREUR TABLE DES SYMBOLES : ");
-	    System.err.println(e.getMessage());
-	    System.exit(e.getCode());
-	}
-	catch(Exception e){}
-    }
 
 	@Override
 	// DEC -> fct id LDEC LDEC LINST
 	public Void visit(SaDecFonc node) throws Exception
 	{
 		defaultIn(node);
-		if(context != Context.GLOBAL){
-				throw new Exception("Le contexte n'est pas global.");
+		this.tableLocaleCourante = new Ts();
+		String identif = node.getNom();
+		Type typeRetour = node.getTypeRetour();
+		int nbArgs = 0;
+
+
+		SaLDecVar maillon;
+		for(maillon = node.getParametres(), nbArgs = 0; maillon != null; maillon = maillon.getQueue(), nbArgs++ );
+
+		if(this.tableGlobale.getFct(identif) != null)
+			throw new ErrorException(Error.TS, "Function already exist.");
+		node.tsItem = tableGlobale.addFct(identif, typeRetour, nbArgs, tableLocaleCourante, node);
+
+		this.context = Context.PARAM;
+		if(node.getParametres() != null)
+			node.getParametres().accept(this);
+
+		this.context = Context.LOCAL;
+		if(node.getVariable() != null){
+			node.getVariable().accept(this);
 		}
-		TsItemFct item = tableGlobale.getFct(node.tsItem.getIdentif());
-		if(item != null){
-			throw new Exception("La fonction est deja existante.");
+
+		if(node.getCorps() != null){
+			node.getCorps().accept(this);
 		}
-		node.tsItem = item;
-		tableGlobale.addFct(node.getNom(), node.getTypeRetour(), node.getParametres().length(), tableGlobale, node);
-		if(node.getParametres() != null) node.getParametres().accept(this);
-		if(node.getVariable() != null) node.getVariable().accept(this);
-		if(node.getCorps() != null) node.getCorps().accept(this);
+		this.context = Context.GLOBAL;
 		defaultOut(node);
 		return null;
 	}
 
 	@Override
 	// DEC -> var id taille
-	public Void visit(SaDecTab node) throws Exception{
+	public Void visit(SaDecTab node) throws Exception {
 		defaultIn(node);
-		TsItemVar item =null;
-		if(context == Context.GLOBAL){
-			item = tableGlobale.getVar(node.getNom());
-			if( item != null){
-				throw new ErrorException(Error.TS, "Variable globale deja utilisée");
-			}
-			node.tsItem = item;
-			tableGlobale.addTab(node.getNom(), node.getType(), node.getTaille());
-		}
-
-		else{
-			item = tableLocaleCourante.getVar(node.getNom());
-			if(item != null){
-				throw new ErrorException(Error.TS, "Variable locale deja utilisée");
-			}
-			node.tsItem = item;
-			if(context == Context.PARAM){
-				tableLocaleCourante.addParam(node.getNom(), node.getType());
-			}
-			else{
-				tableLocaleCourante.addTab(node.getNom(), node.getType(), node.getTaille());
-			}
+		String identif = node.getNom();
+		if(this.context == Context.GLOBAL){
+			if(this.tableGlobale.getVar(identif) != null)
+				throw new ErrorException(Error.TS, "Variable already exist.");
+			node.tsItem = tableGlobale.addTab(identif, node.getType(), node.getTaille());
 		}
 		defaultOut(node);
 		return null;
@@ -87,97 +85,78 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
 
 	@Override
 	// DEC -> var id
-    public Void visit(SaDecVar node) throws Exception {
+	public Void visit(SaDecVar node) throws Exception {
 		defaultIn(node);
-		TsItemVar item = null;
-		if(context == Context.GLOBAL){
-			item = tableGlobale.getVar(node.getNom());
-			if( item != null){
-				throw new ErrorException(Error.TS, "Variable globale deja utilisée");
-			}
-			node.setTsItem(item);
-			tableGlobale.addVar(node.getNom(), node.getType());
+		String identif = node.getNom();
+		if(this.context == Context.GLOBAL){
+			if(tableGlobale.getVar(identif) != null)
+				throw new ErrorException(Error.TS, "Variable already exist.");
+			tableGlobale.addVar(identif, node.getType());
 		}
-		else{
-			item = tableLocaleCourante.getVar(node.getNom());
-			if(item != null){
-				throw new ErrorException(Error.TS, "Variable locale deja utilisée");
-			}
-			node.setTsItem(item);
-			if(context == Context.PARAM){
-				tableLocaleCourante.addParam(node.getNom(), node.getType());
-			}
-			else{
-				tableLocaleCourante.addVar(node.getNom(), node.getType());
-			}
-		}
-		defaultOut(node);
-		return null;
-    }
-
-	@Override
-	public Void visit(SaVarSimple node) throws Exception
-	{
-		defaultIn(node);
-		TsItemVar item = null;
-		if(context == Context.GLOBAL){
-			item = tableGlobale.getVar(node.getNom());
-			if(item == null ){
-				throw new ErrorException(Error.TS, "Variable globale pas trouvée");
-			}
-			node.tsItem = (TsItemVarSimple) item;
-		}
-
-		else{
-			item = tableLocaleCourante.getVar(node.getNom());
-			if(item == null){
-				throw new ErrorException(Error.TS, "Variable locale pas trouvée");
-			}
-			node.tsItem = (TsItemVarSimple) item;
+		else {
+			if(tableLocaleCourante.getVar(identif) != null)
+				throw new ErrorException(Error.TS, "Variable already exist.");
+			if(this.context == Context.PARAM)
+				tableLocaleCourante.addParam(identif, node.getType());
+			else
+				tableLocaleCourante.addVar(identif, node.getType());
 		}
 		defaultOut(node);
 		return null;
 	}
 
 	@Override
-	//todo
+	public Void visit(SaVarSimple node) throws Exception
+	{
+		defaultIn(node);
+		String identif = node.getNom();
+		if(tableLocaleCourante.getVar(identif) != null){
+			if(!(tableLocaleCourante.getVar(identif) instanceof TsItemVarSimple))
+				throw new ErrorException(Error.TS, "Should have index");
+			node.tsItem = (TsItemVarSimple) tableLocaleCourante.getVar(identif);
+		}
+		else if(tableGlobale.getVar(identif) != null){
+			if(!(tableGlobale.getVar(identif) instanceof TsItemVarSimple))
+				throw new ErrorException(Error.TS, "Should have index");
+			node.tsItem = (TsItemVarSimple) tableGlobale.getVar(identif);
+		} else
+			throw new ErrorException(Error.TS, "Var not found");
+
+		defaultOut(node);
+		return null;
+	}
+
+	@Override
 	public Void visit(SaVarIndicee node) throws Exception
 	{
 		defaultIn(node);
-		TsItemVar item = null;
-		if(context == Context.GLOBAL){
-			item = tableGlobale.getVar(node.getNom());
-			if(item == null ){
-				throw new ErrorException(Error.TS, "Variable globale pas trouvée");
-			}
-			node.tsItem = item;
+		TsItemVar item;
+		item = tableGlobale.getVar(node.getNom());
+		if(item == null ){
+			throw new ErrorException(Error.TS, "Var not found");
 		}
+		node.tsItem = item;
 
-		else{
-			item = tableLocaleCourante.getVar(node.getNom());
-			if(item == null){
-				throw new ErrorException(Error.TS, "Variable locale pas trouvée");
-			}
-			node.tsItem = item;
-		}
 		node.getIndice().accept(this);
 		defaultOut(node);
 		return null;
 	}
 
 	@Override
-	//todo
 	public Void visit(SaAppel node) throws Exception
 	{
 		defaultIn(node);
-
-		/*Toute variable utilisée est déclarée en tant que (recherche dans l’ordre) :
-— (1) Variable locale ou (2) argument de fonction ou (3) variable globale.
-— Les tableaux ne peuvent pas être utilisés sans indice
-— Les variables simples ne peuvent pas être indicés
-— Aucune conversion ou coercition n’est possible
-		 */
-		if(node.getArguments() != null) node.getArguments().accept(this);
+		TsItemFct item = null;
+		String identif = node.getNom();
+		int nbArg = node.getArguments().length();
+		item = tableGlobale.getFct(identif);
+		if( item != null){
+			node.tsItem = item;
+			if(nbArg != tableGlobale.getFct(identif).getNbArgs())
+				throw new ErrorException(Error.TS, "Wrong number of arguments");
+		}
+		else
+			throw new ErrorException(Error.TS, "Function does not exist");
 		defaultOut(node);
 		return null;
 	}
